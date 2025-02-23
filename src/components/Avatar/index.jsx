f788127f3c734534cf72f351eceaa843d24b86e6
fmacedosantos <felipemacedo986@gmail.com>
2025-02-23 18:27:08 -0300

    Estilizando o card

--- src/components/Avatar/index.jsx
+++ src/components/Avatar/index.jsx
@@ -1,8 +1,9 @@
 import Image from "next/image"
+import styles from './avatar.module.css'
 
 export const Avatar = ({ name, imageSrc }) => {
     return (
-        <ul>
+        <ul className={styles.avatar}>
             <li>
                 <Image src={imageSrc} width={32} height={32} alt={`Imagem do(a) ${name}`} />
             </li>
