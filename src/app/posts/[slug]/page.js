c390b28ec76518b5f1ff2a44c347f2827ac050bd
fmacedosantos <felipemacedo986@gmail.com>
2025-02-23 22:40:32 -0300

    Lidando com os parâmetros do next

--- src/app/posts/[slug]/page.js
+++ src/app/posts/[slug]/page.js
@@ -1,6 +1,26 @@
-const PagePost = () => {
+import logger from "@/logger"
+
+async function getPostBySlug(slug) {
+    const url = `http://localhost:3042/posts?slug=${slug}`
+    const response = await fetch(url)
+    if (!response.ok) {
+        logger.error('Ops, alguma coisa correu mal')
+        return {}
+    }
+    logger.info('Posts obtidos com sucesso')
+    const data = await response.json()
+    if (data.length == 0) {
+        return {}
+    }
+
+    return data[0]
+}
+
+const PagePost = async ({ params }) => {
+    const post = await getPostBySlug(params.slug)
+
     return (
-        <h1>Posts</h1>
+        <h1>{post.title}</h1>
     )
 }
 
